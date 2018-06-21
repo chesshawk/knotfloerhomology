34bc251c2d421060463cfc5035507c69f6acfcce
Joseph Freund <Joseph@dyn-160-39-149-207.dyn.columbia.edu>
2018-06-21 11:34:31 -0400

    This is my code

---
+++ Project.py
@@ -0,0 +1,82 @@
+sequence = []
+#import a signed sequence
+# get the max of the sequence in absolute value
+def get_max(list):
+    maximum = 0
+    for i in list:
+        if abs(i) > maximum:
+            maximum = i
+    print(maximum)
+    return abs(maximum)
+sequence = [1,2,3, -4]
+get_max(sequence)
+
+#supposing notation is that i corresponds to a short crossing between i and i + 1
+
+#if max = i then number of boundary points is 2(i+1)
+boundary_points = 2*(get_max(sequence)+1)
+print(boundary_points)
+
+tangles = len(sequence) + boundary_points
+print(tangles)
+
+#further tangle decomposition
+
+crossing_tangles = len(sequence) #number of tangles with a cross
+
+caps = len(sequence) + 1
+
+coordinates = []
+def make_grid(list):
+    for i in range(tangles):
+        for j in range(boundary_points):
+            list.append((i,j))
+            j += 1
+        i += 1
+make_grid(coordinates)
+# number of coordinates is tangles times boundaries
+_matrix = [0] * len(coordinates)
+for i in range(len(coordinates)):
+    _matrix[i] = [0] * len(coordinates)
+ 
+def get_entry(x, y): #gets entry in the list of coordinates for the knot
+    if 0 <= x < tangles and 0 <= y < boundary_points:
+        x = boundary_points * x
+        return x + y
+    else:
+        print("Out of Bounds") #come back and figure this out with exceptions
+def get_back(z): #inverse of get_entry
+    if z > boundary_points * tangles:
+        print("Too high, bruh")
+    x = z//boundary_points
+    y = z%boundary_points
+    return (x,y)
+get_entry(13,8)
+get_back(get_entry(13,8))
+for i in range(caps): #coding in the caps data
+    _matrix[get_entry(i,i)][get_entry(i, boundary_points - i - 1)] = 1
+    _matrix[get_entry(i, boundary_points - i - 1)][get_entry(i,i)] = -1
+    _matrix[get_entry(tangles - 1 - i, boundary_points - i - 1)][get_entry(tangles - 1 - i, i)] = 1
+    _matrix[get_entry(tangles - 1 - i,i)][get_entry(tangles - 1 - i, boundary_points - i - 1)] = -1
+for i in range(tangles * boundary_points):
+    for j in range(tangles * boundary_points):
+        if _matrix[i][j] != 0:
+            print (get_back(i), get_back(j), _matrix[i][j])
+        j = j + 1
+    i = i + 1    
+    
+"""
+j = caps
+k = 0
+while j < tangles - caps:
+    i = j - caps
+    _matrix[get_entry(j, boundary_points - sequence[i])][get_entry(j + 1, boundary_points - sequence[i] - 1)] = 1
+    _matrix[get_entry(j + 1, boundary_points - sequence[i] - 1)][get_entry(j, boundary_points - sequence[i])] = -1
+    j = j + 1
+    k = k + 1
+"""
+
+
+
+    
+    
