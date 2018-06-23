df9b05998702a69dea5aee40232fb6a3540b5f4e
Joseph Freund <Joseph@dyn-160-39-149-207.dyn.columbia.edu>
2018-06-23 16:29:15 -0400

    Started to code the multiplcation in the algebra generated by partial bijections. Have the multiplcation defined and have some methods of parsing through the list of all bijections a bit more easily. Could still use some cleanup but thought it might be good to send out now

--- Project.py
+++ Project.py
@@ -1,3 +1,4 @@
+import itertools
 sequence = []
 #import a signed sequence
 # get the max of the sequence in absolute value
@@ -7,7 +8,7 @@ def get_max(list):
         if abs(i) > maximum:
             maximum = abs(i)
     return maximum
-sequence = [1, -2] #This is where you tell the program what crossings are present in the knot's tangle decomposition
+sequence = [1] #This is where you tell the program what crossings are present in the knot's tangle decomposition
 #If any element in the above array is zero I will find you and I will kill you. 
 
 #supposing notation is that i corresponds to a short crossing between i and i + 1
@@ -101,12 +102,87 @@ for i in range(len(sequence)):
         crossing_coord.append((caps - 1 + i + 0.25, boundary_points + sequence[i] - 0.5))
 print (crossing_coord)
 #Tells if crossing is over or under
-def is_over(x):
+def is_positive(x):
     if sequence[x - 1] > 0:
         return True
     else:
         return False
-print (is_over(2))
+
+s = []
+for i in range(boundary_points):
+    s.append(i)
+def findsubsets(S,m):
+    return set(itertools.permutations(S,m))
+a = []
+for i in range(len(s) + 1):
+    a.append(list(findsubsets(s,i)))
+
+def sub_bijections_list(b):
+    bijlist = []
+    for i in range(len(b)):
+        for j in range(len(b)):
+            bij = list(itertools.zip_longest(b[i],b[j]))
+            bijlist.append(bij)
+    return bijlist
+ 
+def all_bijections (a):
+    all_bij = []
+    for i in range(len(a)):
+        all_bij.append(sub_bijections_list(a[i]))
+    return all_bij
+# The ordered pairs have left coordinates domains and right coordinates images.
+"""
+need code to parse bijections better
+"""
+# Coding the multiplcation of algebra elements. I.e. the concatenations.
+
+def domain (a): #For domain need to input a list of tuples or all_bijections then 2 indices. Returns a set. 
+    domain = []
+    for i in range(len(a)):
+        domain.append(list(a[i])[0])
+    domain = set(domain)
+    return domain
+def image (a): #Similar to domain method
+    image = []
+    for i in range(len(a)):
+        image.append(list(a[i])[1])
+    image = set(image)
+    return image
+"""
+rewrite the above methods so that you do not need to index the full bijections list
+"""
+
+print (all_bijections(a)[2][4])
+print (domain(all_bijections(a)[2][4]))
+print (image(all_bijections(a)[2][4]))
+
+def alg_mult (a, b): #imput is two elements of all_bij a[number of elements][bijection]
+    product = []
+    if len(a) != len(b):
+        return 0
+    elif image(a) != domain(b):
+        return 0
+    else:
+        for i in range(len(a)):
+            product.append((list(a[i])[0],list(b[i])[1]))
+    return product
+               
+print (all_bijections(a)[2][0])
+print (all_bijections(a)[2][1])
+print (alg_mult(all_bijections(a)[2][0], all_bijections(a)[2][1]))
+print (all_bijections(a)[2].index(alg_mult(all_bijections(a)[2][0], all_bijections(a)[2][1])))
+
+def get_index(element): #Inputs an element in the algebra (list of tuples) and outputs the index in the list all bijections. Returns a tuple.
+    return (len(element),all_bijections(a)[len(element)].index(element))
+print(get_index(alg_mult(all_bijections(a)[2][0], all_bijections(a)[2][1])))
+print (get_index([(0,1),(1,2),(2,3)]))
+def get_element(x, y): #Given a tuple of positive integers returns the corresponding bijection.
+    if x >= len(all_bijections(a)) or y >= len(all_bijections(a)[x]):
+        print ("nope")
+    else:
+        return all_bijections(a)[x][y]
+print (get_element(3,91))
+
 
 
     
